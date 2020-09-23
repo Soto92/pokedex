574650d61e3f716c4d75a52252eac788e82e26b4
Jonathan Sedrez <jonathansedrez@gmail.com>
2020-09-23 20:41:50 -0300

    add requests to getInitialProps

--- next-app/pages/pokemon/[id].tsx
+++ next-app/pages/pokemon/[id].tsx
@@ -1,26 +1,6 @@
-import {useRouter} from 'next/router'
-import {API_URL} from '../api/pokedex'
-import { useEffect, useState} from 'react'
+import { NextPageContext } from "next";
 
-const PokemonDetails = (props: any) => {
-  const {query} = useRouter()
-  const [pokemon, setPokemon] = useState<{ name: string, stats: any[] }>();
-
-  useEffect(() => {
-    const fetchDetails = async() => {
-      try {
-        console.log(query.id)
-        const response = await fetch(`http://localhost:3000/api/pokedex/${query.id}`)
-        const details = await response.json();
-        setPokemon(details)
-      } catch(error) {
-        
-      }
-    }
-    
-    fetchDetails();
-  },[])
-
+const PokemonDetails = ({ pokemon }: any) => {
   return (
     <div>
      {
@@ -29,13 +9,29 @@ const PokemonDetails = (props: any) => {
           <span> Pokemon { pokemon?.name } </span>
           <h4>Stats:</h4>
           <ul>
-            {pokemon && pokemon?.stats?.map(stat => <li>{stat.stat.name}: {stat.base_stat}</li>)}
+            {pokemon && pokemon?.stats?.map( stat => 
+              <li key={stat.stat.id}>{stat.stat.name}: {stat.base_stat}</li>
+            )}
           </ul>
         </>
       )
     }
     </div>
   )
+}
+
+PokemonDetails.getInitialProps = async ({ query }: NextPageContext) => {
+  try {
+    const response = await fetch(`http://localhost:3000/api/pokedex/${query.id}`)
+    const pokemon = await response.json();
+    return { pokemon }
+  } catch(error) {
+    const { response } = error
+
+    const resultError = response ? response.statusText : error.message
+
+    return resultError
+  }
 }
 
 
