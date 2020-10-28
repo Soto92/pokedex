12345f20a17563af4a4d69f3b71500fd1f81df6d
Bruno Scherer <bruno.raphael96@gmail.com>
2020-10-28 20:09:12 -0300

    feat(angular): pokemon details

--- angular-app/src/app/modules/pokemon/container/pokemon/pokemon.component.ts
+++ angular-app/src/app/modules/pokemon/container/pokemon/pokemon.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { PokedexService } from '../../../../services/pokedex.service';
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
   styleUrls: ['./pokemon.component.scss'],
 })
-export class PokemonComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute) {}
+export class PokemonComponent implements OnInit, OnDestroy {
+  pokemon: any;
+  cancel = new Subject();
+
+  constructor(private activatedRoute: ActivatedRoute, private service: PokedexService) {}
 
   ngOnInit(): void {
     const { id } = this.activatedRoute.snapshot.params;
-    console.log({ id });
+    this.service.getById(id).pipe(takeUntil(this.cancel)).subscribe({
+      next: response => {
+        this.pokemon = response;
+      },
+      error: () => {
+        console.log('request failed');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.cancel.next();
+    this.cancel.complete();
   }
 }
--- angular-app/src/app/services/pokedex.service.ts
+++ angular-app/src/app/services/pokedex.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+
+import { PokemonList, PokemonListItem } from '../interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +12,11 @@ import { environment } from '../../environments/environment';
 export class PokedexService {
   constructor(private http: HttpClient) {}
 
-  getPokemons(limit: number = 10): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}/pokemon?limit=${limit}`);
+  getPokemons(limit: number = 10): Observable<PokemonList> {
+    return this.http.get<PokemonList>(`${environment.apiUrl}/pokemon?limit=${limit}`);
+  }
+
+  getById(id: number): Observable<any> {
+    return this.http.get<any>(`${environment.apiUrl}/pokemon/${id}`);
   }
 }
