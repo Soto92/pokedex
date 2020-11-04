b87a732cb592ef53cef5650a08a34f6434f81859
Mauricio <mauricio.pezoa@ilegra.com>
2020-11-04 19:59:50 -0300

    list pokemons

--- svelte-app/src/routes/Home.svelte
+++ svelte-app/src/routes/Home.svelte
@@ -1,1 +1,19 @@
-<h1>Home</h1>
+<script>
+    import { onMount } from "svelte";
+
+    let pokemons = [];
+    onMount(async function () {
+        const response = await fetch(
+            "https://pokeapi.co/api/v2/pokemon?limit=10"
+        );
+        const json = await response.json();
+        pokemons = json.results;
+    });
+</script>
+
+<h1>Choose your Pokémon</h1>
+<ul>
+    {#each pokemons as pokemon}
+        <li>{pokemon.name}</li>
+    {/each}
+</ul>
