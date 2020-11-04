5600043e60db4966f727a5e5c88c708dac00f11d
Mauricio <mauricio.pezoa@ilegra.com>
2020-11-04 19:37:07 -0300

    init svelte app

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { Router, Route } from "svelte-routing";
+	import Home from "./routes/Home.svelte";
+	export let url = "";
+</script>
+
+<Router {url}>
+	<div>
+		<Route path="/">
+			<Home />
+		</Route>
+	</div>
+</Router>
